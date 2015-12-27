f659f1621fcdf5fe1e4d61e61ab72efca1b8931e
avckp <avck.tara@gmail.com>
2015-12-27 23:06:33 +0530

    Translated to hindi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+app.use('/hi',express.static(__dirname + '/src/hi'));
 app.use(express.static(__dirname + '/src'));
 
 app.listen(process.env.PORT || 5532);
